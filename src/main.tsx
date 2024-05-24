a5975ec924f1dc95b69fe292ba366282771899cd
Moita <katakenshi@icloud.com>
2024-05-24 19:11:06 -0300

    feat: setup route for individual article

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from '@/pages'
+import { Article, Home } from '@/pages'
 import './globals.scss'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -11,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/:id',
+    element: <Article />,
   },
 ])
 
