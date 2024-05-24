659c86a8a0d70d640be58b0f0c1f35f40e214ae6
Moita <katakenshi@icloud.com>
2024-05-24 02:55:16 -0300

    feat: create Ads component

---
+++ src/components/Ads/Ads.tsx
@@ -0,0 +1,24 @@
+import style from './Ads.module.scss'
+
+type AdsProps = {
+  bgImageURL?: string
+}
+
+const Ads = ({ bgImageURL }: AdsProps): JSX.Element => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col col-12">
+          <div
+            className={style.box}
+            style={bgImageURL ? { backgroundImage: `url(${bgImageURL})` } : {}}
+          >
+            {!bgImageURL && <h2 className={style.adsHeading}>Publicidade</h2>}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { Ads }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export * from './Ads'
 export * from './Navbar'
 export * from './icons'
