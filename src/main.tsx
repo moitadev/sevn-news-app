e837ea7b46ef05cf24476f4f170fa9e31291b409
Moita <katakenshi@icloud.com>
2024-05-23 23:02:18 -0300

    feat: setup the router from react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Home } from '@/pages'
 import './globals.scss'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <RouterProvider router={router} />
+  </React.StrictMode>
 )
