11dddf0b81a887727a182dcb77e3a5fb68883e10
Moita <katakenshi@icloud.com>
2024-05-24 13:30:55 -0300

    feat: update home with temporary information

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,28 @@
-import { Ads, Navbar } from '@/components'
+import { Ads, MainNews, Navbar, NewsCard, NewsList } from '@/components'
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Navbar home />
       <Ads />
+      <main className="container">
+        <div className="row row-spacing">
+          <div className="col col-6">
+            <MainNews />
+          </div>
+          <div className="col col-3">
+            <NewsCard />
+          </div>
+          <div className="col col-3">
+            <NewsCard />
+          </div>
+        </div>
+        <div className="row row-spacing">
+          <div className="col col-12">
+            <NewsList />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
