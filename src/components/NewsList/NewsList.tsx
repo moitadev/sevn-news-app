4e8d7618cf91f40c12ed7517b8300babe80c1935
Moita <katakenshi@icloud.com>
2024-05-24 15:28:10 -0300

    refactor: update newslist to support dynamic content

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,19 +1,22 @@
 import style from './NewsList.module.scss'
+import { useQuery } from '@tanstack/react-query'
+import type { Article } from '@/Types'
+import { fetchSecondaryArticles } from '@/fetchers/articles'
 import { NewsListItem } from './NewsListItem'
 
 const NewsList = (): JSX.Element => {
-    return (
-        <ul className={style.NewsList}>
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-            <NewsListItem />
-        </ul>
-    )
+  const { data: secondaryArticles } = useQuery<Article[]>({
+    queryKey: ['secondaryArticles'],
+    queryFn: () => fetchSecondaryArticles(),
+  })
+
+  return (
+    <ul className={style.NewsList}>
+      {secondaryArticles?.map((article, index) => (
+        <NewsListItem key={index} article={article} />
+      ))}
+    </ul>
+  )
 }
 
 export { NewsList }
--- src/components/NewsList/NewsListItem/NewsListItem.tsx
+++ src/components/NewsList/NewsListItem/NewsListItem.tsx
@@ -1,20 +1,20 @@
 import { Link } from 'react-router-dom'
 import style from './NewsListItem.module.scss'
+import { NewsProps } from '@/Types'
 
 const removeAccents = (str: string) => {
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
   }
 
-const NewsListItem = (): JSX.Element => {
-    const category = 'Economia'
-    const normalizedCategory = removeAccents(category).toLowerCase()
+const NewsListItem = ({article}: NewsProps): JSX.Element => {
+    const normalizedCategory = removeAccents(article.category).toLowerCase()
 
   return (
     <li className={style.listItem}>
       <div className={`category-bg-${normalizedCategory} ${style.borderLeft}`}></div>
       <h5>
-        <Link to="/3">
-          Quem não tiver valores a receber poderá ter nas próximas fases, diz BC
+        <Link to={`/${article.id}`}>
+          {article.title}
         </Link>
       </h5>
     </li>
