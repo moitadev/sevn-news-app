53d17519424c4de9b02b9293269902cce212a153
Moita <katakenshi@icloud.com>
2024-05-24 12:20:16 -0300

    style: update color based on category name

--- src/components/MainNews/MainNews.tsx
+++ src/components/MainNews/MainNews.tsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom'
 import style from './MainNews.module.scss'
 
-const MainNews = ():JSX.Element => {
-    return (
-        <div className={style.mainNews}>
-            <small>Economia</small>
-            <h1><Link to="/1">Quem não tiver valores a receber nesta poderá ter nas próximas fases, diz BC</Link></h1>
-        </div>
-    )
+const removeAccents = (str: string) => {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+}
+
+const MainNews = (): JSX.Element => {
+  const category = 'Economia'
+  const normalizedCategory = removeAccents(category).toLowerCase()
+
+  return (
+    <div className={style.mainNews}>
+      <small className={`category-title-${normalizedCategory}`}>Economia</small>
+      <h1>
+        <Link to="/1">
+          Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases,
+          diz BC
+        </Link>
+      </h1>
+    </div>
+  )
 }
 
 export { MainNews }
--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom'
 import { Camera } from '../icons'
 import style from './NewsCard.module.scss'
 
+const removeAccents = (str: string) => {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+}
+
 const NewsCard = (): JSX.Element => {
-  const bgImageURL = 'https://conteudo.imguol.com.br/c/entretenimento/a7/2021/04/12/aula-na-pandemia-covid-escola-1618259816196_v2_900x506.jpg'
+  const bgImageURL =
+    'https://conteudo.imguol.com.br/c/entretenimento/a7/2021/04/12/aula-na-pandemia-covid-escola-1618259816196_v2_900x506.jpg'
+  const category = 'Educação'
+  const normalizedCategory = removeAccents(category).toLowerCase()
 
   return (
     <div className={style.card}>
@@ -17,7 +24,7 @@ const NewsCard = (): JSX.Element => {
         </div>
       </Link>
       <div className={style.cardBody}>
-        <small>Educação</small>
+        <small className={`category-title-${normalizedCategory}`}>Educação</small>
         <h4>
           <Link to="/2">
             Datafolha: após ensino remoto, 76% precisam de reforço na
